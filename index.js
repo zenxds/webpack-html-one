b118b670deb41fd904b3e1d6f324ac2c06c04e05
zenxds <zenxds@gmail.com>
2017-07-01 16:32:26 +0800

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const webpackSources = require('webpack-sources')
+const cheerio = require('cheerio')
+const minify = require('html-minifier').minify
+
+class Plugin {
+  constructor(options) {
+    this.options = Object.assign({
+      minify: true
+    }, options || {})
+  }
+
+  apply(compiler) {
+    const options = this.options
+
+    compiler.plugin("emit", function(compilation, callback) {
+      const assets = compilation.assets
+      const names = Object.keys(assets)
+      const html = names.find(name => /\.html$/i.test(name))
+      const css = names.find(name => /\.css$/i.test(name))
+      const js = names.find(name => /\.js$/i.test(name))
+
+      if (!html) {
+        callback()
+        return
+      }
+
+      const $ = cheerio.load(assets[html].source())
+      const $head = $('head')
+      const $body = $('body')
+      if (css) {
+        $head.append(`<style>${assets[css].source()}</style>`)
+        delete assets[css]
+      }
+      if (js) {
+        $body.append(`<script>${assets[js].source()}</script>`)
+        delete assets[js]
+      }
+
+      assets[html] = new webpackSources.RawSource(options.minify ? minify($.html(), {
+        collapseWhitespace: true
+      }) : $.html())
+      callback()
+    })
+  }
+}
+
+module.exports = Plugin
